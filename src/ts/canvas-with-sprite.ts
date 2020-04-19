91735c698771f20a3e61bd5d32f13a9945737c77
Bryan Alexander Condor de la Cruz <bryanalexandercd@gmail.com>
2020-04-19 00:23:51 -0500

    empezando a trabajar con camvas y sprites (animacion)

---
+++ src/ts/canvas-with-sprite.ts
@@ -0,0 +1,57 @@
+// const guia = document.getElementById('guia') as HTMLElement;
+// for (let index = 0; index < 280; index++) { guia.innerHTML += '<p></p>' }
+
+// const lienzo = document.getElementById('lienzo') as HTMLCanvasElement;
+// const ctx = lienzo.getContext('2d') as CanvasRenderingContext2D;
+// ctx.globalCompositeOperation = 'overlay';
+
+
+// const pintarImage = (posicionImgen: number = 0) => {
+//     ctx.beginPath();
+//     const boyImage = new Image();
+//     boyImage.src = 'assets/image/my_sprite_person.png';
+//     boyImage.onload = () => {
+
+//         ctx.clearRect(0, 0, 1000, 700)
+
+//         // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+//         const widthPartial = boyImage.width / 8;
+//         ctx.drawImage(boyImage, widthPartial * posicionImgen, 0, widthPartial, boyImage.height, 0, 0, widthPartial, boyImage.height);
+//         ctx.stroke();
+
+
+//     }
+// }
+
+// // recursividad para llamar 60 fps
+
+// let contador = 0;
+// let cambio = 0;
+// const inicio = 2;
+// const fin = 5;
+// // const inicio = 2;
+// // const fin = 6;
+
+
+// function log() {
+//     // console.log('contador: ', contador)
+
+
+//     if (contador % 7 === 0) {
+//         cambio++
+//         if (cambio === fin) {
+//             cambio = inicio;
+//         }
+//     }
+
+//     if (cambio >= inicio && cambio <= fin) {
+//         pintarImage(cambio);
+//     }
+
+//     if (contador % 60 === 0) { contador = 0 }
+//     ++contador;
+
+//     window.requestAnimationFrame(log);
+// }
+
+// window.requestAnimationFrame(log);
---
+++ src/ts/index.ts
@@ -0,0 +1,61 @@
+const guia = document.getElementById('guia') as HTMLElement;
+for (let index = 0; index < 280; index++) { guia.innerHTML += '<p></p>' }
+
+const lienzo = document.getElementById('lienzo') as HTMLCanvasElement;
+const ctx = lienzo.getContext('2d') as CanvasRenderingContext2D;
+console.log('ctx :', ctx);
+
+// RECTANGULO
+ctx.fillStyle = 'pink';
+ctx.fillRect(800, 500, 100, 100);
+
+// CIRCULO
+ctx.beginPath();
+ctx.arc(100, 100, 50, 0, 2 * Math.PI);
+ctx.fillStyle = 'skyblue';
+ctx.fill();
+ctx.stroke();
+
+// CIRCULO 2
+ctx.beginPath();
+ctx.arc(75, 325, 25, 0, 2 * Math.PI);
+ctx.fillStyle = 'yellow';
+ctx.fill();
+ctx.strokeStyle = 'orange'
+ctx.stroke();
+
+// CIRCULO CON GRADIENTE
+ctx.beginPath();
+ctx.arc(250, 50, 50, 0, 2 * Math.PI);
+const gradient = ctx.createLinearGradient(300, 0, 100, 0);
+gradient.addColorStop(0, 'red');
+gradient.addColorStop(1, 'blue');
+ctx.fillStyle = gradient;
+ctx.fill();
+ctx.stroke();
+
+// LINEA
+ctx.beginPath()
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5;
+ctx.moveTo(0, 0);
+ctx.lineTo(100, 100);
+ctx.lineTo(200, 100);
+ctx.lineTo(200, 200);
+ctx.stroke();
+
+ctx.beginPath()
+ctx.moveTo(300, 300);
+ctx.lineTo(400, 300);
+ctx.strokeStyle = 'purple';
+ctx.stroke();
+
+// IMAGEN (MAPA DE BITS)
+ctx.beginPath();
+const image = new Image();
+image.src = './chikorita.png';
+image.onload = () => {
+    ctx.drawImage(image, 50, 400, image.width / 1.5, image.height / 1.5);
+    ctx.stroke();
+}
+
